b18df7db64b0572529b4007ee7616390cb84e7c4
Florencia Judith Delgado <florencia.delgado@neoris.com>
2022-10-15 16:16:23 -0300

    Version 1

---
+++ actividad-tp3/src/main/java/ar/edu/unnoba/poo2022/actividad/tp3/ActividadTp3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2022.actividad.tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadTp3Application.class, args);
+	}
+
+}
---
+++ actividad-tp3/src/test/java/ar/edu/unnoba/poo2022/actividad/tp3/ActividadTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2022.actividad.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
